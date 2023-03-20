034b629542d37dc2975c68dfaff2c9a74f8cd715
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-03-20 15:50:55 +0200

    home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,37 +11,11 @@
 <script>
 import LandingHome from "@/components/Home/LandingHome.vue";
 import BrandSection from "@/components/Home/BrandSection.vue";
-// import ProductDetails from "@/pages/ProductDetails.vue";
 import SalingSection from '@/components/Home/SalingSection.vue';
 import HighestRating from '@/components/Home/HighestRating.vue';
 import LastAdd from '@/components/Home/LastAdd.vue';
-// import axios from "axios";
-
 export default {
   components: { LandingHome, BrandSection, SalingSection, HighestRating, LastAdd },
-  data() {
-    return {
-      data: null,
-    };
-  },
-  // created() {
-  //   this.getdata();
-  // },
-  // methods: {
-  //   getdata() {
-  //     this.axios({
-  //       method: "GET",
-  //       url: "brands",
-  //     })
-  //       .then((res) => {
-  //         console.log(res);
-  //       })
-  //       .catch((err) => {
-  //         console.log("thiserrr");
-  //         console.log(err);
-  //       });
-  //   },
-  // },
 };
 </script>
 
