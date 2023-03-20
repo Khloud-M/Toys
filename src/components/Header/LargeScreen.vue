4d9bb0412df045d95c1388de9678f36f81703206
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-03-20 13:29:12 +0200

    header component

---
+++ src/components/Header/LargeScreen.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="large_nav">
+    <div class="top_header">
+      <div class="icon_categery">
+        <button
+          class="button-cart"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample"
+        >
+          <h3>
+            <v-icon icon=" mdi-dots-horizontal"> </v-icon>
+            {{ $t("navs.Categories") }}
+          </h3>
+        </button>
+        <CategeryCart />
+      </div>
+      <ul>
+        <router-link to="/offer"> {{ $t("navs.offer") }}</router-link>
+        <router-link to="/about"> {{ $t("navs.aboutUs") }}</router-link>
+        <router-link to="/Contact"> {{ $t("navs.contactUs") }}</router-link>
+        <change-lang />
+      </ul>
+    </div>
+    <!-- end top header -->
+    <hr />
+    <div class="bottom_header">
+      <search-categery />
+      <ul>
+        <router-link to="/">
+          <v-icon icon="  mdi-home-outline"> </v-icon>
+          {{ $t("navs.home") }}
+        </router-link>
+        <router-link to="/:auth">
+          <v-icon icon="mdi-account"> </v-icon>
+          {{ $t("buttons.signIn") }}</router-link
+        >
+        <v-icon size="27" icon="mdi-bell"></v-icon>
+        <button
+          class="cart_overlay"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasRight"
+          aria-controls="offcanvasRight"
+        >
+          <v-icon size="27" icon="mdi-shopping-outline"></v-icon>
+          <span class="overlay"> {{ cart.length }}</span>
+        </button>
+        <TheCart />
+      </ul>
+    </div>
+    <!-- end bottom header  -->
+  </div>
+</template>
+
+<script>
+import ChangeLang from "@/components/layout/Header/ChangeLang.vue";
+import CategeryCart from "@/components/Home/CategeryCart.vue";
+import TheCart from "@/components/Cart/TheCart.vue";
+import SearchCategery from "./SearchCategery.vue";
+import { mapGetters } from "vuex";
+export default {
+  components: { ChangeLang, CategeryCart, TheCart, SearchCategery },
+  computed: {
+    ...mapGetters({
+      cart: "products/cart",
+    }),
+  },
+};
+</script>
+
+<style lang="scss">
+.large_nav {
+  .top_header,
+  .bottom_header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 7px 0;
+    ul {
+      display: flex;
+      flex-direction: row;
+      column-gap: 15px;
+      align-items: center;
+    }
+  }
+  .top_header {
+    .icon_categery {
+      cursor: pointer;
+      transition: all var(--transtion) ease-in-out;
+      &:hover h3,
+      &:hover .v-icon {
+        color: var(--color-yellow);
+      }
+      h3 {
+        color: var(--main-color);
+        font-size: 15px;
+        transition: all var(--transtion) ease-in-out;
+        .v-icon {
+          color: var(--main-color);
+          transition: all var(--transtion) ease-in-out;
+        }
+      }
+    }
+    a {
+      transition: all var(--transtion) ease-in-out;
+      &:hover {
+        color: var(--color-yellow);
+      }
+    }
+  }
+  .bottom_header {
+    a:hover {
+      color: var(--main-color);
+    }
+  }
+  .cart_overlay{
+    position: relative;
+    .overlay{
+       background-color: yellow;
+       position: absolute;
+       left: 8px;
+       bottom: -12px;
+       font-size: 13px;
+       width: 20px;
+       height: 20px;
+       border-radius: 50%;
+       text-align: center;
+    }
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,82 @@
 <template>
   <v-container class="fill-height">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <div class="phone">
+      <a
+        class="btn btn-primary"
+        data-bs-toggle="offcanvas"
+        href="#offcanvasExample"
+        role="button"
+        aria-controls="offcanvasExample"
+      >
+        Link with href
+      </a>
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasExample"
+        aria-controls="offcanvasExample"
+      >
+        Button with data-bs-target
+      </button>
+
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+          <button
+            type="button"
+            class="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <div>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </div>
+          <div class="dropdown mt-3">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+            >
+              Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
   </v-container>
 </template>
 
 <script setup>
-  //
+//
 </script>
+<style >
+@media (max-width:1000px) {
+  .phone{
+    display: block;
+  }
+
+
+}
+@media (min-width:1001px) {
+  .phone{
+    display: none;
+  }
+
+
+}
+</style>
